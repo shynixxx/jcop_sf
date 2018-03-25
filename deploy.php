6d1ed88b963591e0c60ed8dba0fc720fdb41739c
Jordan <chunlky@hotmail.fr>
2018-03-25 15:48:22 +0200

    Add circle ci

---
+++ deploy.php
@@ -0,0 +1,27 @@
+<?php
+namespace Deployer;
+require 'recipe/symfony.php';
+// Project name
+set('application', 'jcop_sf');
+// Project repository
+set('ssh_type', 'native');
+set('ssh_multiplexing', false);
+set('repository', 'git@github.com:shynixxx/jcop_sf.git');
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', false);
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+// Writable dirs by web server
+add('writable_dirs', []);
+// Hosts
+host('dev2.ylly.fr')
+    ->user('ylly')
+    ->set('deploy_path', '/var/www/p/circle_ci_test');
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+// Migrate database before symlink new release.
+before('deploy:symlink', 'database:migrate');
