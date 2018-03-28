4d60f354b29f9b20521e47d0d72dc7217850d9d3
JWestelynck <jw@ylly.fr>
2018-03-28 16:17:28 +0200

    Add deploy Ok

--- deploy.php
+++ deploy.php
@@ -1,7 +1,7 @@
 <?php
 namespace Deployer;
 
-require 'recipe/symfony.php';
+require 'recipe/symfony3.php';
 
 // Project name
 set('application', 'jcop_sf');
@@ -11,19 +11,9 @@ set('ssh_multiplexing', false);
 set('repository', 'git@github.com:shynixxx/jcop_sf.git');
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', false);
-// Shared files/dirs between deploys
-add('shared_files', []);
-add('shared_dirs', []);
-// Writable dirs by web server
-add('writable_dirs', []);
 // Hosts
 host('dev2.ylly.fr')
     ->user('ylly')
     ->set('deploy_path', '/var/www/p/circle_ci_test');
-task('build', function () {
-    run('cd {{release_path}} && build');
-});
-// [Optional] if deploy fails automatically unlock.
+
 after('deploy:failed', 'deploy:unlock');
-// Migrate database before symlink new release.
-before('deploy:symlink', 'database:migrate');
